f375859c6b018cf980b54401012ea26c51decb13
WEBBOgrafico <e.bonardi@me.com>
2024-10-06 18:00:08 +0200

    tweaked models and added them to django-admin (for #12)

--- projects/admin.py
+++ projects/admin.py
@@ -1,1 +1,18 @@
 # Register your models here.
+from django.contrib import admin
+from projects.models import Project, Tech, Image
+
+
+@admin.register(Project)
+class SchoolAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tech)
+class TechAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Tech(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Technology"
+        verbose_name_plural = "Technologies"
+
     def __str__(self):
         return self.name
 
@@ -17,16 +21,12 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     hero_image = models.ImageField(upload_to="img/projects/")
-    images = models.ManyToManyField(
-        Image, related_name="projects", blank=True, null=True
-    )
+    images = models.ManyToManyField(Image, related_name="projects", blank=True)
     live_url = models.URLField()
     source_code_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    technologies = models.ManyToManyField(
-        Tech, related_name="projects", blank=True, null=True
-    )
+    technologies = models.ManyToManyField(Tech, related_name="projects", blank=True)
 
     def __str__(self):
         return self.title
